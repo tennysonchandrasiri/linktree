5e31edc6202a33fc2041b5d54bbbd20fc923e467
tennysonchandrasiri <89110045+tennysonchandrasiri@users.noreply.github.com>
2023-02-27 12:57:02 +0530

    Update content.ts

--- app/content.ts
+++ app/content.ts
@@ -1,52 +1,64 @@
-export const codeRepository = "linktree"
-export const githubUsername = "pruthivithejan"
-export const heading = "Pruthivi Thejan | Linktree"
+export const codeRepository = "linktree";
+export const githubUsername = "tennysonchandrasiri";
+export const heading = "Tennyson Chandrasiri | Linktree";
 export const slogan = {
-    words: "I'll be myself or",
-    hightlight: "die trying",
-}     
-        
+  words: "I'm a",
+  hightlight: "Father & a Teacher",
+};
+
 export const Cards = [
-    {
-        href: "https://mobile.twitter.com/pruthivithejan",
-        icon: 'twitter',
-        title: 'Twitter' ,
-        description : 'I care about Twitter, I hope Elon Musk will not ruin it.',
-    },
-    {
-        href: "https://www.instagram.com/pruthivithejan/",
-        icon: 'instagram',
-        title: 'Instagram' ,
-        description : 'When I take good photos, I post it here.',
-    },
-    {
-        href: "https://www.reddit.com/user/pruthivithejan/",
-        icon: 'reddit',
-        title: 'Reddit' ,
-        description : 'For most Q&A, I use reddit.',
-    },
-    {
-        href: "https://t.me/pruthivithejan/",
-        icon: 'telegram',
-        title: 'Telegram' ,
-        description : 'Telegram is my most used messaging app.',
-    },
-    {
-        href: "https://link.friendspire.com/2TCHVXcy8ub",
-        icon: 'friendspire',
-        title: 'Friendspire' ,
-        description : 'Movies, TV Shows, Books and Podcasts are rated here.',
-    },   
-    {
-        href: "https://open.spotify.com/user/cipqdclr942xwcn7tdeo14o06?si=14e86fca3b8e462d",
-        icon: 'spotify',
-        title: 'Spotify' ,
-        description : 'My Spotify playlists are here.'
-    },   
-    {
-        href: "https://github.com/pruthivithejan",
-        icon: 'github',
-        title: 'Github' ,
-        description : 'I upload all my codes here.',
-    },   
-]
+  {
+    href: "https://www.youtube.com/@tennysonchandrasiri/videos",
+    icon: "youtube",
+    title: "YouTube",
+    description: "Subscribe to my YouTube channel.",
+  },
+  {
+    href: "https://facebook.com/tennysonsir",
+    icon: "facebook",
+    title: "Facebook",
+    description: "Follow my Facebook page.",
+  },
+  {
+    href: "https://m.me/tennysonsir",
+    icon: "messenger",
+    title: "Messenger",
+    description: "All the questions can be asked here.",
+  },
+  {
+    href: "https://mobile.twitter.com/tennysonsir",
+    icon: "twitter",
+    title: "Twitter",
+    description: "You can find my latest Tweets here.",
+  },
+  {
+    href: "https://www.instagram.com/tennysonchandrasiri/",
+    icon: "instagram",
+    title: "Instagram",
+    description: "Follow me on Instagram.",
+  },
+  {
+    href: "https://t.me/tennysonsir/",
+    icon: "telegram",
+    title: "Telegram",
+    description: "Subscribe to my Telegram Channel.",
+  },
+  {
+    href: "https://open.spotify.com/user/31pu4d7oupimc7c3ijnehga34hre?si=6aa46fc2c876467d",
+    icon: "spotify",
+    title: "Spotify",
+    description: "My Spotify playlists are here.",
+  },
+  {
+    href: "https://github.com/tennysonchandrasiri",
+    icon: "github",
+    title: "Github",
+    description: "You can find the code to this Linktree here.",
+  },
+  {
+    href: "mailto:chandrasiri.tennyson@gmail.com",
+    icon: "gmail",
+    title: "Gmail",
+    description: "Business Inquiries Only.",
+  },
+];
